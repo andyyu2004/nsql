49ce84f599d4ab31227d537d20c2f64ffeaf89d7
Andy Yu <andyyu2004@gmail.com>
2023-06-24 20:00:04 +1200

    start adding benchmarks

---
+++ nsql/benches/nsql.rs
@@ -0,0 +1,28 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use nsql::Nsql;
+use nsql_redb::RedbStorageEngine;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("insert");
+
+    for size in [100, 1000, 10000, 100000] {
+        group.throughput(Throughput::Elements(size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
+            b.iter(|| {
+                let db_path = nsql_test::tempfile::NamedTempFile::new().unwrap().into_temp_path();
+                let nsql = Nsql::<RedbStorageEngine>::create(db_path).unwrap();
+                let (conn, state) = nsql.connect();
+                conn.query(&state, "CREATE TABLE t (id int PRIMARY KEY)").unwrap();
+                conn.query(
+                    &state,
+                    &format!("INSERT INTO t SELECT * FROM UNNEST(range(1, {}))", size),
+                )
+                .unwrap();
+            });
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
