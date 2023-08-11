3382f1ce7911473c814978b78dc804e3e3029865
Andy Yu <andyyu2004@gmail.com>
2023-08-11 17:40:03 +1200

    refactor visitor trait to use controlflow rather than booleans

--- nsql-ir/src/visit.rs
+++ nsql-ir/src/visit.rs
@@ -1,3 +1,5 @@
+use std::ops::ControlFlow;
+
 use crate::{Expr, ExprKind, Join, JoinConstraint, Plan};
 // FIXME returning ControlFlow is probably better than using bool these days, also lets the visitor
 // carry a value on break which is convenient (can also use `?` for ergonomics)
@@ -11,93 +13,116 @@ pub trait Visitor {
     /// Any transformation must be type-preserving
 
     #[inline]
-    fn visit_plan(&mut self, plan: &Plan) -> bool {
+    fn visit_plan(&mut self, plan: &Plan) -> ControlFlow<()> {
         self.walk_plan(plan)
     }
 
-    fn walk_plan(&mut self, plan: &Plan) -> bool {
+    fn walk_plan(&mut self, plan: &Plan) -> ControlFlow<()> {
         match plan {
-            Plan::Show(_typ) => false,
-            Plan::Drop(_refs) => false,
-            Plan::Transaction(_tx) => false,
-            Plan::CreateNamespace(_info) => false,
-            Plan::CreateTable(_info) => false,
-            Plan::SetVariable { .. } => false,
-            Plan::TableScan { table: _, projection: _, projected_schema: _ } => false,
+            Plan::Show(_typ) => ControlFlow::Continue(()),
+            Plan::Drop(_refs) => ControlFlow::Continue(()),
+            Plan::Transaction(_tx) => ControlFlow::Continue(()),
+            Plan::CreateNamespace(_info) => ControlFlow::Continue(()),
+            Plan::CreateTable(_info) => ControlFlow::Continue(()),
+            Plan::SetVariable { .. } => ControlFlow::Continue(()),
+            Plan::TableScan { table: _, projection: _, projected_schema: _ } => {
+                ControlFlow::Continue(())
+            }
             Plan::Projection { source, projection, projected_schema: _ } => {
-                self.visit_plan(source) || self.visit_exprs(projection)
+                self.visit_plan(source)?;
+                self.visit_exprs(projection)
             }
             Plan::Filter { source, predicate } => {
-                self.visit_plan(source) || self.visit_expr(predicate)
+                self.visit_plan(source)?;
+                self.visit_expr(predicate)
             }
             Plan::Unnest { expr, schema: _ } => self.visit_expr(expr),
-            Plan::Values { values: _, schema: _ } => false,
+            Plan::Values { values: _, schema: _ } => ControlFlow::Continue(()),
             Plan::Join { schema: _, join, lhs, rhs } => {
-                self.visit_plan(lhs)
-                    || self.visit_plan(rhs)
-                    || match join {
-                        Join::Inner(constraint)
-                        | Join::Left(constraint)
-                        | Join::Right(constraint)
-                        | Join::Full(constraint) => self.visit_join_constraint(constraint),
-                        Join::Cross => false,
-                    }
+                self.visit_plan(lhs)?;
+                self.visit_plan(rhs)?;
+                match join {
+                    Join::Inner(constraint)
+                    | Join::Left(constraint)
+                    | Join::Right(constraint)
+                    | Join::Full(constraint) => self.visit_join_constraint(constraint),
+                    Join::Cross => ControlFlow::Continue(()),
+                }
             }
             Plan::Limit { source, limit: _ } => self.visit_plan(source),
             Plan::Order { source, order } => {
-                self.visit_plan(source)
-                    || order.iter().any(|order_expr| self.visit_expr(&order_expr.expr))
+                self.visit_plan(source)?;
+                for order_expr in &order[..] {
+                    self.visit_expr(&order_expr.expr)?;
+                }
+                ControlFlow::Continue(())
             }
-            Plan::Empty => false,
+            Plan::Empty => ControlFlow::Continue(()),
             Plan::Explain(plan) => self.visit_plan(plan),
             Plan::Insert { table: _, source, returning, schema: _ } => {
-                self.visit_plan(source)
-                    || returning.as_ref().map_or(false, |returning| self.visit_exprs(returning))
+                self.visit_plan(source)?;
+                if let Some(returning) = returning {
+                    self.visit_exprs(returning)?;
+                }
+                ControlFlow::Continue(())
             }
             Plan::Update { table: _, source, returning, schema: _ } => {
-                self.visit_plan(source)
-                    || returning.as_ref().map_or(false, |returning| self.visit_exprs(returning))
+                self.visit_plan(source)?;
+                if let Some(returning) = returning {
+                    self.visit_exprs(returning)?;
+                }
+                ControlFlow::Continue(())
             }
             Plan::Aggregate { source, functions: _, group_by, schema: _ } => {
-                self.visit_plan(source) || self.visit_exprs(group_by)
+                self.visit_plan(source)?;
+                self.visit_exprs(group_by)
             }
         }
     }
 
-    fn visit_join_constraint(&mut self, constraint: &JoinConstraint) -> bool {
+    fn visit_join_constraint(&mut self, constraint: &JoinConstraint) -> ControlFlow<()> {
         match constraint {
             JoinConstraint::On(expr) => self.visit_expr(expr),
-            JoinConstraint::None => false,
+            JoinConstraint::None => ControlFlow::Continue(()),
         }
     }
 
     #[inline]
-    fn visit_exprs(&mut self, exprs: &[Expr]) -> bool {
-        exprs.iter().any(|expr| self.visit_expr(expr))
+    fn visit_exprs(&mut self, exprs: &[Expr]) -> ControlFlow<()> {
+        for expr in exprs {
+            self.visit_expr(expr)?;
+        }
+        ControlFlow::Continue(())
     }
 
     #[inline]
-    fn visit_expr(&mut self, expr: &Expr) -> bool {
+    fn visit_expr(&mut self, expr: &Expr) -> ControlFlow<()> {
         self.walk_expr(expr)
     }
 
-    fn walk_expr(&mut self, expr: &Expr) -> bool {
+    fn walk_expr(&mut self, expr: &Expr) -> ControlFlow<()> {
         match &expr.kind {
-            ExprKind::Literal(_) => false,
+            ExprKind::Literal(_) => ControlFlow::Continue(()),
             ExprKind::Array(exprs) => self.visit_exprs(exprs),
-            ExprKind::BinOp { op: _, lhs, rhs } => self.visit_expr(lhs) || self.visit_expr(rhs),
-            ExprKind::ColumnRef { .. } => false,
+            ExprKind::BinOp { op: _, lhs, rhs } => {
+                self.visit_expr(lhs)?;
+                self.visit_expr(rhs)
+            }
+            ExprKind::ColumnRef { .. } => ControlFlow::Continue(()),
             ExprKind::FunctionCall { function: _, args } => self.visit_exprs(args),
             ExprKind::Alias { alias: _, expr } => self.visit_expr(expr),
             ExprKind::Case { scrutinee, cases, else_result } => {
-                self.visit_expr(scrutinee)
-                    || cases
-                        .iter()
-                        .any(|case| self.visit_expr(&case.when) || self.visit_expr(&case.then))
-                    || else_result
-                        .as_ref()
-                        .as_ref()
-                        .map_or(false, |else_result| self.visit_expr(else_result))
+                self.visit_expr(scrutinee)?;
+                for case in &cases[..] {
+                    self.visit_expr(&case.when)?;
+                    self.visit_expr(&case.then)?;
+                }
+
+                if let Some(else_result) = else_result {
+                    self.visit_expr(else_result)?;
+                }
+
+                ControlFlow::Continue(())
             }
             ExprKind::UnaryOp { op: _, expr } => self.visit_expr(expr),
             ExprKind::Subquery(plan) => self.visit_plan(plan),
