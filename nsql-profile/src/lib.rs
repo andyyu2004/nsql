00e6f7266e2421d0354d7533ea1ce57cb73a16be
Andy Yu <andyyu2004@gmail.com>
2023-11-12 16:14:24 +1300

    track time spent evaluating expressions

--- nsql-profile/src/lib.rs
+++ nsql-profile/src/lib.rs
@@ -24,8 +24,9 @@ pub struct Profiler {
     pub physical_plan_explain_event_id: EventId,
 
     pub execute_event_id: EventId,
+    pub execute_expr_id: EventId,
 
-    thread_id: u32,
+    thread_id: u32, // TODO, once we use multiple threads this won't be right
 }
 
 impl Profiler {
@@ -45,6 +46,7 @@ impl Profiler {
             physical_plan_compile_function_lookup_event_id: mk_id("compile-function-lookup"),
             physical_plan_explain_event_id: mk_id("explain"),
             execute_event_id: mk_id("execute"),
+            execute_expr_id: mk_id("execute-expr"),
             generic_event_kind: profiler.alloc_string("generic"),
             // everything is currently single-threaded and always will be except for execution stuff
             thread_id: std::thread::current().id().as_u64().get() as u32,
@@ -53,11 +55,15 @@ impl Profiler {
     }
 
     pub fn profile<R>(&self, event_id: EventId, f: impl FnOnce() -> R) -> R {
-        let _guard = self.profiler.start_recording_interval_event(
+        let _guard = self.start(event_id);
+        f()
+    }
+
+    pub fn start(&self, event_id: EventId) -> impl Drop + '_ {
+        self.profiler.start_recording_interval_event(
             self.generic_event_kind,
             event_id,
             self.thread_id,
-        );
-        f()
+        )
     }
 }
